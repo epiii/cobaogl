fb51d5dde041466c036ca83e7f75c97d9afbf4fe
elfri anto <elfri anto>
2017-05-08 17:10:17 +0700

    getting started

---
+++ cobaogl/Display.cpp
@@ -0,0 +1,12 @@
+#include "Display.h"
+
+
+
+Display::Display(int width, int height, const std::string& title){
+	
+}
+
+
+Display::~Display()
+{
+}
---
+++ cobaogl/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <GL/glew.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	std::cout << "halo cak" << std::endl;
+	return 0;
+}
